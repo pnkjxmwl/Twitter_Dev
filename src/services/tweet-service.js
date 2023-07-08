087ad0f5e6e070702ba268bd23c2c88a47d5b31d
pankaj <pankajsemwalpankaj123@gmail.com>
2023-07-08 10:02:53 +0530

    added like and basic user model

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -10,7 +10,7 @@ class TweetService{
     async create(data)
     {
         const content=data.content; 
-        console.log(content);
+       
         let tags= content.match(/#[a-zA-Z0-9_]+/g);
         tags= tags.map((tag)=>tag.substring(1))
                     .map(tag=>tag.toLowerCase());
